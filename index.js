933c10e6b585c998e2505e7b9e3a64c81e32b565
Jeff Jewiss <jeffjewiss@gmail.com>
2016-01-07 21:04:41 -0500

    Removes semicolons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var postcss = require('postcss');
-var reporter = require('./lib/reporter');
+var postcss = require('postcss')
+var reporter = require('./lib/reporter')
 
-module.exports = postcss.plugin('postcss-stats-reporter', reporter);
+module.exports = postcss.plugin('postcss-stats-reporter', reporter)
